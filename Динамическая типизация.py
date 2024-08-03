ec412c1fda1a065338bf91d530a4da9d4fab57c3
Юлия <kornyakova_11@mail.ru>
2024-08-03 18:50:16 +0500

    Add files via upload

---
+++ Динамическая типизация.py
@@ -0,0 +1,13 @@
+# ДЗ урок 3 Переменные module_1_3.py
+name = "Yuliya"
+print(name, type(name))
+age = 36
+print(age, type(age))
+new_age = "2"
+print(age+int(new_age))
+print(age+1)
+is_student = "True"
+print(is_student)
+print("is student: ""True")
+is_student = "Yuliya"
+print(is_student == "Yuliya")  # boolean
